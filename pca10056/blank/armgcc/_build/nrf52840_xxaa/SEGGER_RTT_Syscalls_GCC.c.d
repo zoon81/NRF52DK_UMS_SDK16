9e5d52ebb29d912af6116aa47c1922cbd489ddc7
zoon81 <zoon81@gmail.com>
2020-11-12 20:38:27 +0100

    Cleanup

--- pca10056/blank/armgcc/_build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.d
+++
@@ -1,9 +0,0 @@
-_build/nrf52840_xxaa/SEGGER_RTT_Syscalls_GCC.c.o: \
- /home/zoon/Workspace/Nordic/NRF5_SDK_16/nRF5SDK160098a08e2/external/segger_rtt/SEGGER_RTT_Syscalls_GCC.c \
- ../config/sdk_config.h ../config/app_config.h ../config/app_settings.h
-
-../config/sdk_config.h:
-
-../config/app_config.h:
-
-../config/app_settings.h:
--- pca10056/blank/armgcc/_build/nrf52840_xxaa/gcc_startup_nrf52840.S.d
+++
@@ -1,2 +0,0 @@
-_build/nrf52840_xxaa/gcc_startup_nrf52840.S.o: \
- /home/zoon/Workspace/Nordic/NRF5_SDK_16/nRF5SDK160098a08e2/modules/nrfx/mdk/gcc_startup_nrf52840.S
